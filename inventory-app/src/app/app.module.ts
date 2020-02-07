900f123f34533b807f6cfb243b042bcfcd10f5f6
Emir Basic <ebasic@gmail.com>
2020-02-07 18:52:51 +0100

    Revert "inventory project"
    This reverts commit 3b849665d2219b274f26dd8e06d3b227c201663c.

--- inventory-app/src/app/app.module.ts
+++ inventory-app/src/app/app.module.ts
@@ -1,29 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductRowComponent } from './product-row/product-row.component';
-import { ProductImageComponent } from './product-image/product-image.component';
-import { PriceDisplayComponent } from './price-display/price-display.component';
-import { ProductDepartmentComponent } from './product-department/product-department.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductImageComponent,
-    ProductDepartmentComponent,
-    PriceDisplayComponent,
-    ProductRowComponent,
-    ProductListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
